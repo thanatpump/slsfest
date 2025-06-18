1808aab0c44add565ebf184b80278a11ec3c8d60
thanatpump1 <thanat.tcl@kkumail.com>
2025-06-18 11:01:33 +0700

    Initial project upload

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'vzesousztjhoymntujtq.supabase.co',
+        pathname: '/storage/v1/object/public/slsfest-slip/**',
+      },
+    ],
+  },
+}; 
